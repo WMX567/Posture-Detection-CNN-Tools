2eee5b25c7c289b0be20ac93f49237ac0b1393b4
武梦溪 <wumengxi567@126.com>
2018-08-09 21:38:57 -0400

    Update parameter names

--- sensor17marker.py
+++ sensor17marker.py
@@ -1,12 +1,10 @@
 """
-17 markers sensor
-Cal angles tracked by sensor
-return elbow, shoulder, back, knee, neck angles
+Vectors (left&right) for 17 markers sensor
 wumengxi@umich.edu
 """
 
 def calculate_vector_sensor_left_17(x, y, z, bool_values):
-    vec_back, vec_head, vec_left_arm, vec_left_elbow, vec_left_leg, vec_left_knee = ([] for i in range(6))
+    vec_back, vec_head, vec_arm, vec_elbow, vec_leg, vec_knee = ([] for i in range(6))
     if bool_values[0]:
         vec_back = [(x[3]+x[10]-x[0]-x[14])/2, (y[3]+y[10]-y[0]-y[14])/2, (z[3]+z[10]-z[0]-z[14])/2]
     if bool_values[1]:
@@ -23,7 +21,7 @@ def calculate_vector_sensor_left_17(x, y, z, bool_values):
 
 
 def calculate_vector_sensor_right_17(x, y, z, bool_values):
-    vec_back, vec_head, vec_left_arm, vec_left_elbow, vec_left_leg, vec_left_knee = ([] for i in range(6))
+    vec_back, vec_head, vec_arm, vec_elbow, vec_leg, vec_knee = ([] for i in range(6))
     if bool_values[0]:
         vec_back = [(x[3]+x[10]-x[0]-x[14])/2, (y[3]+y[10]-y[0]-y[14])/2, (z[3]+z[10]-z[0]-z[14])/2]
     if bool_values[1]:
