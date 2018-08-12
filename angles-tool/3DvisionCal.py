db8e73435c6efc09c0f2390c66808fe917d5566a
武梦溪 <wumengxi567@126.com>
2018-08-12 16:42:28 -0400

    Update 3DvisionCal.py

--- angles-tool/3DvisionCal.py
+++ angles-tool/3DvisionCal.py
@@ -13,10 +13,12 @@ from Angle_3D_Cal import proccess_3D_angles
 param_path = '/Users/wumengxi/Desktop/Sync_Diag_Parameters_Summary.csv'
 sensor_data_path = '/Users/wumengxi/Desktop/Raw_Data_Sensor/'
 output_path = '/Users/wumengxi/Desktop/3D_Angles/'
-input_path = '/Users/wumengxi/Desktop/Final_Diag_NoTape/'
-sensor_files = ['toyota_2017_05_28_135905_001_3d.csv']
-files = ['FinalSkeleton_20_Diag_0133_S2.txt','FinalSkeleton_20_Diag_0133_S4.txt']
-filenum = '133'
+input_path = '/Users/wumengxi/Desktop/Final_Left_NoTape/'
+sensor_files = ['toyota_new_2017_05_22_145025_001_3d.csv']
+files = ['FinalSkeleton_15_Left_NoTape_0220_S4.txt'] #, 'FinalSkeleton_22_Left_NoTape_0235_S2_2.txt', 'FinalSkeleton_22_Left_NoTape_0235_S2_3.txt']
+filenum = '220'
+
+index_ref = []
 
 def read_linearRegression_parameters(filenum):
     lr_param = []
@@ -30,13 +32,11 @@ def read_linearRegression_parameters(filenum):
 
 def data_selection(sensor_data_path, lr_param):
     x, y, z, X, Y, video_frames = ([] for i in range(6))
-    #Read CNN estimation
     for inputfile in files:
         X_part, Y_part, video_frame = read_model_output(input_path + inputfile)
         X.extend(X_part)
         Y.extend(Y_part)
         video_frames.extend(video_frame)
-    #Read Sensor data
     for filename in sensor_files:
         with open(sensor_data_path + filename, encoding='utf-8', errors='replace') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -55,7 +55,7 @@ def data_selection(sensor_data_path, lr_param):
                 x.append(x_temp)
                 y.append(y_temp)
                 z.append(z_temp)
-    x_final, y_final, z_final, index_ref = ([] for i in range(4))
+    x_final, y_final, z_final = ([] for i in range(3))
     for i in range(len(video_frames)):
         index_match = int(lr_param[0] * video_frames[i][0] + lr_param[1])
         x_final.append(x[index_match-1])
@@ -70,6 +70,7 @@ def data_selection(sensor_data_path, lr_param):
         writer.writerows(index_ref)
     return x_final, y_final, z_final, X, Y
 
+
 def replace_invalid_data(x, y, z, is_2D):
     for i in range(len(x)):
         for j in range(len(x[i])):
@@ -83,21 +84,21 @@ def replace_invalid_data(x, y, z, is_2D):
                 z[i][j] = 0
     return x, y, z
 
+
 def cnn_sensor_diff_evaluation(lr_param, is_record):
     angle_3D_cal, angle_3D_truth, vectors_cal, vectors_truth = ([] for i in range(4))
     mean_diff = [0]*5
     num_values_used = [0]*5
     x, y, z, X, Y = data_selection(sensor_data_path, lr_param)
     x, y, z = replace_invalid_data(x, y, z, True)
-    for i in range(0,len(X)):
-        print('Frame:', i)
+    for i in range(0,len(X),6):
+        print('index_match:', index_ref[i])
         angles, vectors = proccess_3D_angles(X[i], Y[i], z[i], False)
         angle_3D_cal.append(angles)
         vectors_cal.append(vectors)
         angles, vectors = proccess_3D_angles(x[i], y[i], z[i], True)
         angle_3D_truth.append(angles)
         vectors_truth.append(vectors)
-    print('Length of X:', len(X))
     for i in range(len(angle_3D_truth)):
         for j in range(5):
             if angle_3D_truth[i][j] != -4111:
@@ -110,37 +111,23 @@ def cnn_sensor_diff_evaluation(lr_param, is_record):
         with open(output_path + 'truth.csv', 'w') as output:
             writer = csv.writer(output, lineterminator='\n')
             writer.writerows(angle_3D_truth)
+        print(len(angle_3D_truth))
         for i in range(len(num_values_used)):
             print('Retrieve Rate:', num_values_used[i]/len(angle_3D_truth))
     return mean_diff, num_values_used
+
 
 if __name__ == "__main__":
     lr = read_linearRegression_parameters(filenum)
     print('linear parameter:', lr)
     mean_diff, num_values_used = cnn_sensor_diff_evaluation(lr, True)
+    print(mean_diff)
+    print(num_values_used)
     for i in range(len(num_values_used)):
-        mean_diff[i] = mean_diff[i] / num_values_used[i]
+        if num_values_used[i] != 0:
+            mean_diff[i] = mean_diff[i] / num_values_used[i]
     print('Angle Accuracy:')
     print(mean_diff)
-   """for i in range(-5, 5):
-        for j in range(-5, 5):
-            lr_param = [lr[0]+i*0.01, lr[1]+j*5]
-            mean_diff, num_values_used = cnn_sensor_diff_evaluation(lr_param, False)
-            meanDiffs.append(mean_diff)
-            num_used.append(num_values_used)
-    for i in range(len(num_used)):
-        for j in range(len(num_used[i])):
-            meanDiffs[i][j] = meanDiffs[i][j] / num_used[i][j]
-    means = [np.mean(angles) for angles in meanDiffs]
-    print('Means', means)
-    minPos = means.index(min(means))
-    print('Min Position:', minPos)
-    best_fit_param = [lr[0]+minPos*0.01, lr[1]+minPos*5]"""
-
-
-
-
-
 
 
 
