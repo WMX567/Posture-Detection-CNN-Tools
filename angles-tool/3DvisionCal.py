d7706dbc1b714ef55e7b54b8d8316fec0f9d53a0
武梦溪 <wumengxi567@126.com>
2018-08-13 17:48:59 -0400

    Update 3DvisionCal.py

--- angles-tool/3DvisionCal.py
+++ angles-tool/3DvisionCal.py
@@ -7,16 +7,16 @@ wumengxi@umich.edu
 """
 import os, csv
 import numpy as np
-from ReadModelOutput import read_model_output
+from ReadModelOuput import read_model_output
 from Angle_3D_Cal import proccess_3D_angles
 
-param_path = '/Users/wumengxi/Desktop/Sync_Diag_Parameters_Summary.csv'
-sensor_data_path = '/Users/wumengxi/Desktop/Raw_Data_Sensor/'
-output_path = '/Users/wumengxi/Desktop/3D_Angles/'
-input_path = '/Users/wumengxi/Desktop/Final_Left_NoTape/'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+param_path = os.path.join(dir_path,'Sync_Diag_Parameters_Summary.csv')
+sensor_data_path = os.path.join(dir_path, 'Raw_Data_Sensor')
+output_path = os.path.join(dir_path, '3D_Angles')
+input_path = os.path.join(dir_path, 'Final_Diag_NoTape')
 sensor_files = ['toyota_new_2017_05_22_145025_001_3d.csv']
-files = ['FinalSkeleton_15_Left_NoTape_0220_S4.txt']
-filenum = '220'
+files = ['FinalSkeleton_15_Diag_NoTape_0802_S4.txt'] 
 
 index_ref = []
 
@@ -38,7 +38,7 @@ def data_selection(sensor_data_path, lr_param):
         Y.extend(Y_part)
         video_frames.extend(video_frame)
     for filename in sensor_files:
-        with open(sensor_data_path + filename, encoding='utf-8', errors='replace') as csvfile:
+        with open(os.path.join(sensor_data_path, filename), encoding='utf-8', errors='replace') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 x_temp, y_temp, z_temp = ([] for i in range(3))
@@ -62,10 +62,10 @@ def data_selection(sensor_data_path, lr_param):
         y_final.append(y[index_match-1])
         z_final.append(z[index_match-1])
         index_ref.append([index_match-1])
-    with open(output_path + 'video_frames.csv', 'w') as output:
+    with open(os.path.join(output_path, 'video_frames.csv'), 'w') as output:
         writer = csv.writer(output, lineterminator='\n')
         writer.writerows(video_frames)
-    with open(output_path + 'index_ref.csv', 'w') as output:
+    with open(os.path.join(output_path, 'index_ref.csv'), 'w') as output:
         writer = csv.writer(output, lineterminator='\n')
         writer.writerows(index_ref)
     return x_final, y_final, z_final, X, Y
@@ -105,13 +105,12 @@ def cnn_sensor_diff_evaluation(lr_param, is_record):
                 mean_diff[j] = mean_diff[j] + np.absolute(angle_3D_cal[i][j] - angle_3D_truth[i][j])
                 num_values_used[j] = num_values_used[j] + 1
     if is_record == True:
-        with open(output_path + 'cal.csv', 'w') as output:
+        with open(os.path.join(output_path,'cal.csv'), 'w') as output:
             writer = csv.writer(output, lineterminator='\n')
             writer.writerows(angle_3D_cal)
-        with open(output_path + 'truth.csv', 'w') as output:
+        with open(os.path.join(output_path,'truth.csv'), 'w') as output:
             writer = csv.writer(output, lineterminator='\n')
             writer.writerows(angle_3D_truth)
-        print(len(angle_3D_truth))
         for i in range(len(num_values_used)):
             print('Retrieve Rate:', num_values_used[i]/len(angle_3D_truth))
     return mean_diff, num_values_used
@@ -128,6 +127,4 @@ if __name__ == "__main__":
             mean_diff[i] = mean_diff[i] / num_values_used[i]
     print('Angle Accuracy:')
     print(mean_diff)
-
-
 
