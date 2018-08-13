da631e9ae51def56cb80f6022c89e8071084c42d
武梦溪 <wumengxi567@126.com>
2018-08-13 18:05:09 -0400

    Update sample6.py

--- angles-tool/sample6.py
+++ angles-tool/sample6.py
@@ -1,16 +1,21 @@
 """
-Sample the posture codes calculated from CNN output to match the groud truth file
+Sample the posture codes to match the groud truth file
+wumengxi@umich.edu
+"""
+"""
+Sample the posture codes to match the groud truth file
 wumengxi@umich.edu
 """
 import os,csv
 import pandas as pd
 
-file_path = '/Users/wumengxi/Desktop/Posture_Codes/'
-output_path = '/Users/wumengxi/Desktop/Sample_Results'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.join(dir_path,'PostureCodes')
+output_path = os.path.join(dir_path,'Sample_Results')
 
 posture_files = os.listdir(file_path)
 for filename in posture_files:
-	posture_codes = pd.read_csv(file_path + filename, encoding = 'utf8').dropna(how='all')
+	posture_codes = pd.read_csv(os.path.dirname(file_path, filename), encoding = 'utf8').dropna(how='all')
 	posture_codes = posture_codes.values.tolist()
 	posture_sampled = []
 	read_length = int(len(posture_codes)/6)
@@ -19,3 +24,5 @@ for filename in posture_files:
 		writer = csv.writer(output, lineterminator='\n')
 		writer.writerows(posture_sampled)
 
+		
+		
