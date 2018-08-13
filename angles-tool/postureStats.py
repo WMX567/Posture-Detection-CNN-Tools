3df4c57d4dc3f60c4d6e11b59797d7036d9aff2f
武梦溪 <wumengxi567@126.com>
2018-08-13 15:29:12 -0400

    Update postureStats.py

--- angles-tool/postureStats.py
+++ angles-tool/postureStats.py
@@ -8,32 +8,48 @@ import os,sys,csv
 import pandas as pd
 import numpy as np
 from pycm import ConfusionMatrix
+from ReadModelOutput import modify_filename
 
-truth_path = '/Users/wumengxi/Desktop/Ground_T/'
-vision_path = '/Users/wumengxi/Desktop/Vision_Tests/'
-output_path = '/Users/wumengxi/Desktop/Confusion_Matrix/'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+vision_path = dir_path + '/PostureCodes/'
+truth_path = dir_path + '/Ground_Truth/'
+output_path = dir_path + '/Confusion_Matrix/'
+param_path = dir_path + '/truth_correspond.csv'
 
-def count_postureCode(filename):
-    read_truth = pd.read_csv(truth_path + filename, encoding = 'utf8').dropna(how='all')
-    read_vision = pd.read_csv(vision_path + filename, encoding = 'utf8').dropna(how='all')
-    truth = read_truth.values.tolist()
-    vision = read_vision.values.tolist()
-    return truth, vision
+def mapping(vision_filename):
+    video_ID = ''
+    subject_ID = int(vision_filename[0:2])
+    video_cut = vision_filename[-1]
+    print('video:', video_cut)
+    if len(vision_filename) > 10:
+        video_cut = vision_filename[-3:]
+    with open(param_path, encoding='utf-8', errors='replace') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['Subject ID'] != '':
+                if int(row['Subject ID']) == subject_ID:
+                    video_ID = row['Video ID']
+                    break
+    pad_zero = '0'
+    if len(video_ID) < 3:
+        pad_zero = '00'
+    truth_filename = vision_filename[0:2] + '_' + pad_zero + video_ID + '_' + 'S' + video_cut
+    return truth_filename
 
 if __name__ == "__main__":
-    truth_files = os.listdir(truth_path)
+    vision_files = os.listdir(vision_path)
     upper_limbs_truth, upper_limbs_vision, back_truth, back_vision = ([] for i in range(4))
     leg_truth, leg_vision, neck_truth, neck_vision = ([] for i in range(4))
-    for filename in truth_files:
-        truth, vision = count_postureCode(filename)
-        vision_sampled, video_frames = ([] for i in range(2))
-        for i in range(len(truth)):
-            video_frames.append([i*6])
-            vision_sampled.append(vision[i*6])
-        truth = list(map(list, zip(*truth)))
-        vision = list(map(list, zip(*vision_sampled)))
-        if filename != '.DS_Store':
+    for filename in vision_files:
+        if filename != '.DS_Store' and filename != '.DS_Store.csv':
+            #打散Video的名称
+            #每一个新的Video都有一个新的Directory
+            #根据S2或者S4分类
             print('File used: ', filename)
+            vision_filename = modify_filename(filename)
+            vision_filename = vision_filename.replace('.csv', '')
+            video_ID = vision_filename[3:6]
+            truth, vision = count_postureCode(vision_filename)
             upper_limbs_truth.extend(list(map(int, truth[0])))
             back_truth.extend(list(map(int, truth[1])))
             leg_truth.extend(list(map(int, truth[2])))
@@ -45,15 +61,17 @@ if __name__ == "__main__":
     options = ['Upper Limb','Back','Knee','Neck']
     truth = [upper_limbs_truth, back_truth, leg_truth, neck_truth]
     vision = [upper_limbs_vision, back_vision, leg_vision, neck_vision]
+    #Create new directory
+    new_path = output_path + vision_filename + '_CM/'
+    os.makedirs(new_path)
     for i in range(len(truth)):
         print_stdout = sys.stdout
-        out_file = open(output_path + options[i] + '_CM.txt', 'w')
+        out_file = open(new_path + options[i] + '_CM.txt', 'w')
         sys.stdout = out_file
         con_matrix = ConfusionMatrix(truth[i], vision[i])
         print(con_matrix)
         sys.stdout = print_stdout
         out_file.close()
-
 
 
 
