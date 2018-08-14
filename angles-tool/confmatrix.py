94d7fbe47e67175aaca9643ef15b521e0f58b026
武梦溪 <wumengxi567@126.com>
2018-08-14 15:49:42 -0400

    Update confmatrix.py

--- angles-tool/confmatrix.py
+++ angles-tool/confmatrix.py
@@ -23,10 +23,27 @@ def re_value(a, b):
 		b = 1
 	return a, b
 
-def confusMatrix(actual, pred, option):
+def print_confusion_matrix(matrix, labels, tpr):
+	matrix = np.concatenate((np.array(labels)[:, None], matrix), axis = 1)
+	labels = [str(label) for label in labels]
+	labels = ['T/P'] + labels
+	colwidth = max([len(label) for label in labels]+ [10])
+	print("".join(item.ljust(colwidth) for item in labels))
+	for row in matrix:
+		row = [str(item) for item in row]
+		print("".join(item.ljust(colwidth) for item in row))
+	print('True Positive Rate:')
+	labels[0] = 'Labels:'
+	tpr = [str(item) for item in tpr[0]]
+	tpr = ['TPR:'] + tpr
+	print("".join(item.ljust(colwidth) for item in labels))
+	print("".join(item.ljust(colwidth) for item in tpr))
+	return
+
+def confusion_matrix(actual, pred, option):
 	assert(len(actual) == len(pred))
 	labels = define_label(option)
-	matrix = np.zeros((len(labels),len(labels)))
+	matrix = np.zeros((len(labels),len(labels)), dtype=int)
 	for i in range(len(labels)):
 		for j in range(len(labels)):
 			for act_line, pred_line in zip(actual, pred):
@@ -34,6 +51,7 @@ def confusMatrix(actual, pred, option):
 					act_line[3], pred_line[3] = re_value(act_line[3], pred_line[3])
 				if act_line[option] == i and pred_line[option] == j:
 					matrix[i][j] = matrix[i][j] + 1
+	#计算所需要的数值
 	tpr = np.zeros((1, len(labels)))
 	for i in range(len(labels)):
 		sum = np.sum(matrix[i])
