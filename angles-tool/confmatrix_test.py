b9366fa9e74b60b9f29c49b07f8d65fd3a2d4fe4
武梦溪 <wumengxi567@126.com>
2018-08-14 15:50:36 -0400

    Update confmatrix_test.py

--- angles-tool/confmatrix_test.py
+++ angles-tool/confmatrix_test.py
@@ -1,8 +1,7 @@
-from confmatrix import confusMatrix
+from confmatrix import confusion_matrix, print_confusion_matrix
 
 actual = [[0,1, 2, 0], [1, 2, 3, 1], [0, 2, 1, 1], [4, 1, 2, 2], [1, 2, 3, 0]]
 pred = [[2, 1, 3, 0], [1, 3, 2, 1], [1, 0, 0, 2], [2, 1, 0, 0], [1, 1, 2, 0]]
-conmat, tpr, labels = confusMatrix(actual, pred, 3)
-print(conmat)
-print(labels)
-print(tpr)
+conmat, tpr, labels = confusion_matrix(actual, pred, 3)
+print_confusion_matrix(conmat, labels, tpr)
+
